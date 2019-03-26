df3e5f679c005a647bd2a6ac14e33e7e1a8f3653
minh nguyen <imnotminh@gmail.com>
2019-03-26 05:01:58 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,12 @@
 # Write your code here.
+katz_deli=[]
+
+def line(array)
+  counter=1
+  if array.length==0
+    puts "The line is currently empty."
+  else
+    array.each do |person|
+      puts 
+  end
+end
